07d60933da1e4e124fb48ec80083853038a43c1e
wozsun <master@wozsun.com>
2023-07-20 18:59:02 +0800

    set: fun

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -205,6 +205,11 @@ export default defineUserConfig({
                     maskIcon: "https://asset.wozsun.com/wozsun.svg",
                 }
             },
+            autoCatalog: {
+                level: 3,
+                index: false,
+                exclude: [],
+            },
         },
         blog: {
             sidebarDisplay: "none",
